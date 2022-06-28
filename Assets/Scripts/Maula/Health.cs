a3b0c4e429b29cb2311913a2e31ab47177827a06
ndahMaulid <maulidyandah14@gmail.com>
2022-06-29 05:27:30 +0700

    all file

---
+++ Assets/Scripts/Maula/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+  [SerializeField] GameObject LoseCanvas;
+  [SerializeField] float startingHealth = 3f;
+  public static float currentHealth;
+  public bool dead;
+
+  void Awake()
+  {
+    if (currentHealth == 0)
+    {
+      currentHealth = startingHealth;
+    }
+  }
+
+  public float GetCurrentHealth()
+  {
+    return currentHealth;
+  }
+
+  public void TakeDamage(float damage)
+  {
+    currentHealth = Mathf.Clamp(currentHealth - damage, 0f, startingHealth);
+
+    if (currentHealth <= 0)
+    {
+      if (!dead)
+      {
+        dead = true;
+        LoseCanvas.SetActive(true);
+        FindObjectOfType<UIStartMenu>().Pause();
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/Maula/Healthball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthball : MonoBehaviour
+{
+  Health health;
+  [SerializeField] Image totalHealthbarImage;
+  [SerializeField] Image currentHealthbarImage;
+
+  void Awake()
+  {
+    health = FindObjectOfType<Health>();
+  }
+
+  void Start()
+  {
+    totalHealthbarImage.fillAmount = health.GetCurrentHealth() / 5f;
+  }
+
+  void Update()
+  {
+    currentHealthbarImage.fillAmount = health.GetCurrentHealth() / 5f;
+  }
+}
